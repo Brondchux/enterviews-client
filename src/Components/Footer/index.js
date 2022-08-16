ff08f9c50b310888f1a6f949428acaefacc5a3d2
Gospel Chukwu <brondchux@gmail.com>
2022-08-16 19:01:33 -0400

    Added current year to footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,11 +1,19 @@
 import "./footer.css";
 import constants from "../../Utils/constants";
+import { useEffect, useState } from "react";
 
 const Footer = () => {
+	const [year, setYear] = useState(null);
+
+	useEffect(() => {
+		const date = new Date();
+		setYear(date.getFullYear());
+	}, []);
+
 	return (
 		<footer>
 			<p>
-				&copy; <span id="currentYear"></span>
+				&copy; <span>{year}</span>&nbsp;
 				{constants.BRAND}
 			</p>
 		</footer>
