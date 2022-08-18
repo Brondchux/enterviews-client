480fe66c4dc811d4a45a4476d979691fc5818a47
Gospel Chukwu <brondchux@gmail.com>
2022-08-18 10:28:28 -0400

    Completed designs for not-found page and updated interviews json file

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,5 +1,28 @@
+import "../Styles/NotFound.css";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
 const NotFound = () => {
-	return <h2>NotFound page</h2>;
+	return (
+		<Fragment>
+			<Header />
+			<main className="not-found">
+				<section>
+					<h2>404</h2>
+					<p>
+						It'a a dead end! Click
+						<Link to={{ pathname: "/interviews" }} className="link">
+							&nbsp;here&nbsp;
+						</Link>
+						to get back on track 🙂
+					</p>
+				</section>
+			</main>
+			<Footer />
+		</Fragment>
+	);
 };
 
 export default NotFound;
