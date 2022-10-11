81bfee2058860452c78e0a53f48dd4b002088fa9
Gospel Chukwu <brondchux@gmail.com>
2022-10-11 15:29:16 -0400

    Removed constant API_HOST from interview-services file

--- src/Store/services/interview.js
+++ src/Store/services/interview.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import constants from "../../Utils/constants";
 
-const API_URL = `${constants.API_HOST}/api/interviews`;
+const API_URL = "/api/interviews";
 
 // End interview process service
 const endInterview = async (interviewData, token) => {
